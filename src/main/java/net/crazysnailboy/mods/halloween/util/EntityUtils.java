6d5bf54f4d3f71d6b08a497d4b0d58a678f19d89
Chris Porter <github@crazysnailboy.net>
2017-08-13 12:22:21 +0100

    improvements to curses and treater ai

--- src/main/java/net/crazysnailboy/mods/halloween/util/EntityUtils.java
+++ src/main/java/net/crazysnailboy/mods/halloween/util/EntityUtils.java
@@ -3,7 +3,9 @@ package net.crazysnailboy.mods.halloween.util;
 import java.lang.reflect.Field;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.EnumSkyBlock;
@@ -11,6 +13,56 @@ import net.minecraft.world.EnumSkyBlock;
 
 public class EntityUtils
 {
+
+
+
+	public static boolean getCanSpawnHere(EntityLivingBase entity, EnumCreatureType creatureType)
+	{
+		// default spawning logic for instances of EntityLiving
+		boolean canLivingSpawnHere = entity.world.getBlockState((new BlockPos(entity)).down()).canEntitySpawn(entity);
+		boolean canCreatureSpawnHere = false;
+		boolean canMonsterSpawnHere = false;
+
+		if (canLivingSpawnHere)
+		{
+			// default spawning logic for instances of EntityCreature
+			float blockPathWeight = 0.0F;
+			if (entity instanceof EntityCreature)
+			{
+				BlockPos pos = new BlockPos(entity.posX, entity.getEntityBoundingBox().minY, entity.posZ);
+				blockPathWeight = ((EntityCreature)entity).getBlockPathWeight(pos);
+			}
+			canCreatureSpawnHere = (blockPathWeight >= 0.0F);
+			if (creatureType == EnumCreatureType.CREATURE) return canCreatureSpawnHere;
+		}
+
+		if (canCreatureSpawnHere && creatureType == EnumCreatureType.MONSTER)
+		{
+			// default spawning logic for instances of EntityMob
+			if (entity.world.getDifficulty() != EnumDifficulty.PEACEFUL)
+			{
+				BlockPos pos = new BlockPos(entity.posX, entity.getEntityBoundingBox().minY, entity.posZ);
+				if (entity.world.getLightFor(EnumSkyBlock.SKY, pos) <= entity.world.rand.nextInt(32))
+				{
+					int i = entity.world.getLightFromNeighbors(pos);
+					if (entity.world.isThundering())
+					{
+						int j = entity.world.getSkylightSubtracted();
+						entity.world.setSkylightSubtracted(10);
+						i = entity.world.getLightFromNeighbors(pos);
+						entity.world.setSkylightSubtracted(j);
+					}
+					canMonsterSpawnHere = (i <= entity.world.rand.nextInt(8));
+				}
+			}
+			return canMonsterSpawnHere;
+		}
+
+		return canLivingSpawnHere;
+	}
+
+
+
 
 	/**
 	 * Checks whether the spawn conditions support spawning of creatures.
@@ -27,17 +79,17 @@ public class EntityUtils
 	 * Adapted from net.minecraft.entity.EntityCreature#getCanSpawnHere
 	 *
 	 */
-	public static boolean getCanCreatureSpawnHere(EntityLivingBase entity)
+	private static boolean getCanCreatureSpawnHere(EntityLivingBase entity)
 	{
 		return getCanLivingSpawnHere(entity) && EntityCreature_getBlockPathWeight(new BlockPos(entity.posX, entity.getEntityBoundingBox().minY, entity.posZ)) >= 0.0F;
 	}
 
 	/**
 	 * Checks whether the spawn conditions support spawning of living entities.
-	 * Adapted from net.minecraft.entity.EntityLiving#getCanSpawnHere
+	 * Adapted from {@link net.minecraft.entity.EntityLiving#getCanSpawnHere()}
 	 *
 	 */
-	public static boolean getCanLivingSpawnHere(EntityLivingBase entity)
+	private static boolean getCanLivingSpawnHere(EntityLivingBase entity)
 	{
 		IBlockState state = entity.world.getBlockState((new BlockPos(entity)).down());
 		return state.canEntitySpawn(entity);
@@ -46,7 +98,7 @@ public class EntityUtils
 
 	/**
 	 * Checks to make sure the light is not too bright where the mob is spawning
-	 * Adapted from net.minecraft.entity.monster.EntityMob#isValidLightLevel
+	 * Adapted from {@link net.minecraft.entity.monster.EntityMob#isValidLightLevel()}
 	 *
 	 */
 	private static boolean EntityMob_isValidLightLevel(EntityLivingBase entity)
